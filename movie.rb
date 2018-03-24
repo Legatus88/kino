8975ec60da353ca1d88278e6a42a57fefef417d6
Legatus88 <y1wkn8@gmail.com>
2018-03-24 19:39:36 +0400

    Finished filtering and errors

--- movie.rb
+++ movie.rb
@@ -1,31 +1,38 @@
 require 'csv'
 
-class MyError < StandardError
-end
-
 class Movie
-  attr_accessor :my_hash, :link, :title, :year, :country, :starting_date, :genre, :time, :rate, :producer, :actors  
+  attr_accessor :col, :my_hash, :link, :title, :year, :country, :starting_date, :genre, :time, :rate, :producer, :actors  
  
-  def initialize(hash={})
+  def initialize(hash={}, collection)
+    @col = collection
     @my_hash = hash
     @link = hash[:link]
     @title = hash[:title]
-    @year = hash[:year]
+    @year = hash[:year].to_i
     @country = hash[:country]
     @starting_date = hash[:starting_date]
     @genre = hash[:genre].split(',')
     @time = hash[:time].to_i
-    @rate = hash[:rate]
+    @rate = hash[:rate].to_f
     @producer = hash[:producer]
-    @actors = hash[:actors]	  
+    @actors = hash[:actors].split(',')
   end
 
+  def matches?(key, values)
+      if key.select {|cell| cell.include?(values.to_s)}.length == 0
+        false
+      else 
+        true
+      end
+  end
+
+
   def has_genre?(param)
-    if genre.include?(param) == false
-      raise MyError, 'It doesn\'t exist'
+    if col[:genre].select {|hash| hash.include?(param)}.length != 0
+      genre.include?(param)
     else
-      true
-    end
+      raise 'Sorry, this genre doesn\'t exist'
+    end  
   end
 
 end
--- movie_collection.rb
+++ movie_collection.rb
@@ -2,12 +2,12 @@ require 'csv'
 require './movie'
 
 class MovieCollection
-  attr_accessor :file 
+  attr_accessor :file, :csv_list  
 
   def initialize(file_name)
-    @file = file_name 
-    @full_list = CSV.read(file, :col_sep => "|", :headers => %i[link title year country starting_date genre time rate producer actors]).map {|row|
-    Movie.new(row) }
+    @file = file_name
+    @csv_list = CSV.read(file, :col_sep => "|", :headers => %i[link title year country starting_date genre time rate producer actors])
+    @full_list = csv_list.map { |row| Movie.new(row, self.csv_list) }
   end  
 
 # Выдать список фильмов: all возвращает массив всех фильмов, которые в нём хранятся
@@ -21,12 +21,14 @@ class MovieCollection
   end
 
 # выдать фильтрованный список фильмов — по некоторым полям, вроде жанра и страны, например movies.filter(genre: 'Comedy') — возвращает массив фильмов с жанром «Comedy»;
-  def filter(hash)
-    sorted_list = all 
-    hash.each {|key, values| sorted_list = sorted_list.select{|hash| hash.send(key).include?(values)}}
-    
-
-    sorted_list
+  def filter(parameter)
+    parameter.reduce(all){|result, (key, value)| 
+    result.select{|movie_list| 
+      if value.is_a? String
+        movie_list.matches?(movie_list.send(key), value)
+      else 
+        value === movie_list.send(key) 
+      end }}
   end
 
 # выдать статистику по запросу: режиссёр, актёр, год, месяц, страна, жанр — например, movies.stats(:director) возвращает хеш «имя режиссёра → количество фильмов»
@@ -34,16 +36,4 @@ class MovieCollection
     cutted_arr = all.flat_map(&parameter).sort
     cutted_arr.each_with_object(Hash.new(0)) {|value, list| list[value] += 1 }
   end
-
-# Вот такое выражение: has_genre?('Tragedy') (когда такой жанр вообще не существует) — должно бросать исключение. А код в основном файле должен его перехватывать и печатать, что за исключение произошло
-  def has_genre?(parameter)
-  	final_list = all.map{|hash| hash.genre}.select{|hash| hash.include?(parameter) }.length
-  	if final_list == 0 
-  	  puts true
-  	  throw :stopProc 
-  	else
-  	  puts false
-  	end
-  end
-
 end
