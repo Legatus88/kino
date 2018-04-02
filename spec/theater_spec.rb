bba6fea9a6a45ab37748c9b54ff62f1ea7f354ba
Legatus88 <y1wkn8@gmail.com>
2018-04-02 20:38:39 +0400

    Fixes

--- spec/theater_spec.rb
+++ spec/theater_spec.rb
@@ -2,7 +2,7 @@ require './theater'
 
 
 describe '.when' do
-  it 'give an AncientMovie list' do
+  it 'should give morning time' do
   	oc = Theater.new('movies.txt')
     expect(oc.when?('Citizen Kane')).to be == ['08:00'..'12:00']
   end
