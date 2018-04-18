0c1fbecda52418b4e9ac133da2b05ba32fe5e04a
Legatus88 <y1wkn8@gmail.com>
2018-04-18 15:03:09 +0400

    megre

--- classic_movie.rb
+++ classic_movie.rb
@@ -2,8 +2,7 @@ require 'csv'
 require './movie'
 
 class ClassicMovie < Movie
-  COST = Money.new(150, "USD")
-  
+  COST = Money.new(150, 'USD')
   def description
     "#{title} — классический фильм, режиссёр #{producer}: (ещё #{col.filter(producer: producer).length} фильмов в топе)"
   end
