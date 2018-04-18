8d08862bc4f904751223742f4c3d721d7ecc8a58
Kirill Baranenkov <36744416+Legatus88@users.noreply.github.com>
2018-04-18 15:06:05 +0400

    Update netflix.rb

--- netflix.rb
+++ netflix.rb
@@ -15,8 +15,32 @@ class Netflix < MovieCollection
     Netflix.add_money(coins*100) 
   end
 
-  def show(parameter)
-    list = filter(parameter)
+  def define_filter(name, from: nil, arg: nil, &block)
+    if !from.nil? 
+      @hash[name] = ->(movie) { @hash[from].call(movie, arg) }
+    else
+      @hash = {name => block} 
+    end
+  end 
+
+  def show(parametr={}, &block)
+
+    combined_parametrs = parametr.partition{ |key, value| all.first.my_hash[key] }
+    new_param = combined_parametrs[0].to_h
+    new_block = combined_parametrs[1].to_h
+
+    if new_block.length > 0
+      list = new_block.reduce(filter(new_param)){|result, (key, value)| 
+        result.select{|movie| 
+          if value == true
+            @hash[key].call(movie)
+          else 
+            @hash[key].call(movie, value)
+          end}}.select(&block)
+    else
+      list = filter(new_param).select(&block)
+    end
+
     @coins >= list.first.price or raise ArgumentError, "Need more money"
     @coins -= list.first.price
     random_movie = list.sort_by { |m| m.rate * rand }.last
