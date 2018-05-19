f19ee7ba23116d42ccfe95f81281d8e77111a807
Legatus88 <y1wkn8@gmail.com>
2018-05-19 21:33:20 +0400

    added empty test

---
+++ spec/budget_spec.rb
@@ -0,0 +1,12 @@
+require './budget'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  # Указываем где будем хранить наши кассеты )
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  # Интегрируемся с webmock 
+  config.hook_into :webmock
+end
+
+
